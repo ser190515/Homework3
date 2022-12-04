2c1e8f6e11ea9ed1b49f4bcffa706cec45814397
ser19 <ser190515@gmail.com>
2022-12-04 23:11:11 +0200

    Д.З.3 Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        byte b = 5;
+        System.out.println("значение переменной b с типом byte равно 5");
+        short s = 100;
+        System.out.println("значение переменной s с типом short равно 100");
+        int i = 700;
+        System.out.println("значение переменной i с типом int равно 700");
+     long l = 1000L;
+        System.out.println("значение переменной l с типом long равно 1000");
+     float f = 45.78f;
+        System.out.println("значение переменной f с типом float равно 45.78");
+     double d = 68.42;
+        System.out.println("значение переменной d с типом double равно 68.42");
     }
 }
